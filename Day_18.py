bce918a4787edbb1a770c013821b76cc8b650e7f
yuumi15 <101173359+yuumi15@users.noreply.github.com>
2024-11-12 00:21:57 +0200

    Add files via upload

---
+++ Day_18.py
@@ -0,0 +1,22 @@
+#Day_18
+print("Guess the number game!!\nChoose a numebr between 1 and 1,000,000\n")
+
+num = 77777
+counter = 0
+
+while True:
+  counter +=1
+  ans = int(input("Enter your guess: "))
+  if ans > num:
+    print("Too high")
+  elif ans < num and ans > -1:
+    print("Too low")
+  elif ans == num:
+    print("You got it! 🎉 only in", counter, "attempts")
+    break
+  elif ans < 0:
+    print("Now you'll never know what the answer is > <")
+    exit()
+  else:
+    print("Its not a number I recognize ??")
+  

1222f27e64b9b0cf41977edf289e69f4909b3c7c
yuumi15 <101173359+yuumi15@users.noreply.github.com>
2025-04-20 00:03:50 +0200

    Add files via upload

---
+++ Day_30_fStrings.py
@@ -0,0 +1,10 @@
+#Day 30
+#fStrings
+#Basically learned how to use f strings and why they are so powerfull
+
+for i in range (1, 31):
+  op = input(f"What did you think of day {i} ?")
+  print(f"You thought day {i} was {op}!!\n")
+
+
+

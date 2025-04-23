888882b2883022586974c05e2c8385c9369f7bcd
yuumi15 <101173359+yuumi15@users.noreply.github.com>
2025-04-24 01:52:03 +0200

    Add files via upload

---
+++ Day_32_HelloWorldWide.py
@@ -0,0 +1,9 @@
+#Day 32
+#Hellow in diff languages 
+#This is too easy im definitly sulking 
+
+import random
+
+greetings = ["Hola!", "Konnichiwa~", "Alsalm Alaikum ♥", "Hey, Pretty xo", "Anyonghaseyooo", "Bonjour", "Ni Hao > <", "Hallo", "Alohaaa"]
+
+print(greetings[random.randint(0, len(greetings))])
